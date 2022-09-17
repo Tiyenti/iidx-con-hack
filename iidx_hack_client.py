ef410f85c8141feeb1e45c480e673a1ccb403dcd
Tiyenti <11019859+Tiyenti@users.noreply.github.com>
2022-09-17 21:57:16 +0100

    use inputs library for client, also fix a bug (time.sleep was causing problems with keys sticking for longer than they should)

--- iidx_hack_client.py
+++ iidx_hack_client.py
@@ -18,56 +18,67 @@ inputs.get_gamepad()
 
 s.send(bytearray([0b00000010]))
 
+# codes
+KEY1 = 'BTN_TRIGGER'
+KEY2 = 'BTN_THUMB'
+KEY3 = 'BTN_THUMB2'
+KEY4 = 'BTN_TOP'
+KEY5 = 'BTN_TOP2'
+KEY6 = 'BTN_PINKIE'
+KEY7 = 'BTN_BASE'
+E1 = 'BTN_BASE3'
+E2 = 'BTN_BASE4'
+
+
 while True:
     events = inputs.get_gamepad()
     
     for event in events:
-        if 'eventType' == 'BUTTON':
-            if control == '1':
-                if value:
-                    s.send(bytearray([0b11000001]))
-                else:
-                    s.send(bytearray([0b11100001]))
-            if control == '2':
-                if value:
-                    s.send(bytearray([0b11000010]))
-                else:
-                    s.send(bytearray([0b11100010]))
-            if control == '3':
-                if value:
-                    s.send(bytearray([0b11000011]))
-                else:
-                    s.send(bytearray([0b11100011]))
-            if control == '4':
-                if value:
-                    s.send(bytearray([0b11000100]))
-                else:
-                    s.send(bytearray([0b11100100]))
-            if control == '5':
-                if value:
-                    s.send(bytearray([0b11000101]))
-                else:
-                    s.send(bytearray([0b11100101]))
-            if control == '6':
-                if value:
-                    s.send(bytearray([0b11000110]))
-                else:
-                    s.send(bytearray([0b11100110]))
-            if control == '7':
-                if value:
-                    s.send(bytearray([0b11000111]))
-                else:
-                    s.send(bytearray([0b11100111]))
-            if control == 'E1':
-                if value:
-                    s.send(bytearray([0b11001000]))
-                else:
-                    s.send(bytearray([0b11101000]))
-            if control == 'E2':
-                if value:
-                    s.send(bytearray([0b11001001]))
-                else:
-                    s.send(bytearray([0b11101001]))
+        if event.code == KEY1:
+            if event.state:
+                s.send(bytearray([0b11000001]))
+            else:
+                s.send(bytearray([0b11100001]))
+        if event.code == KEY2:
+            if event.state:
+                s.send(bytearray([0b11000010]))
+            else:
+                s.send(bytearray([0b11100010]))
+        if event.code == KEY3:
+            if event.state:
+                s.send(bytearray([0b11000011]))
+            else:
+                s.send(bytearray([0b11100011]))
+        if event.code == KEY4:
+            if event.state:
+                s.send(bytearray([0b11000100]))
+            else:
+                s.send(bytearray([0b11100100]))
+        if event.code == KEY5:
+            if event.state:
+                s.send(bytearray([0b11000101]))
+            else:
+                s.send(bytearray([0b11100101]))
+        if event.code == KEY6:
+            if event.state:
+                s.send(bytearray([0b11000110]))
+            else:
+                s.send(bytearray([0b11100110]))
+        if event.code == KEY7:
+            if event.state:
+                s.send(bytearray([0b11000111]))
+            else:
+                s.send(bytearray([0b11100111]))
+        if event.code == E1:
+            if event.state:
+                s.send(bytearray([0b11001000]))
+            else:
+                s.send(bytearray([0b11101000]))
+        if event.code == E2:
+            if event.state:
+                s.send(bytearray([0b11001001]))
+            else:
+                s.send(bytearray([0b11101001]))
 
-    time.sleep(0.01)
+    #time.sleep(0.01)
     pass
