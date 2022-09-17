3741f562ca655cb1bcc585bd4bb4aa55a049e084
Tiyenti <11019859+Tiyenti@users.noreply.github.com>
2022-09-17 02:49:08 +0100

    change IP of client so it works from VM

--- iidx_hack_client.py
+++ iidx_hack_client.py
@@ -3,7 +3,7 @@ import socket, time
 ## Client runs on the Linux VM and sends controller inputs to the server
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("localhost", 44717))
+s.connect(("10.0.2.2", 44717))
 
 ## send some data to indicate we've connected
                  # status      1-7 and start e1 and e2     turntable axis float
