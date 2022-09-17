f93d5560969e0b8b5f7808d2a883259c3b04113c
Tiyenti <11019859+Tiyenti@users.noreply.github.com>
2022-09-17 04:54:50 +0100

    code but it's basically all nonfunctional atm server probably works (probably) but the client code, it doesn't look like the Gamepad library is working... so i need to switch to a different library, but the documentation is... uh, not great, so that might take some time to figure out

--- iidx_hack_client.py
+++ iidx_hack_client.py
@@ -1,4 +1,4 @@
-import socket, time
+import socket, time, inputs
 
 ## Client runs on the Linux VM and sends controller inputs to the server
 
@@ -8,9 +8,66 @@ s.connect(("10.0.2.2", 44717))
 ## send some data to indicate we've connected
                  # status      1-7 and start e1 and e2     turntable axis float
 s.send(bytearray([0b00000001, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000]))
+print("Successfully established connection to server.")
+
+a = 0
+
+print("Awaiting controller connection...")
+
+inputs.get_gamepad()
+
+s.send(bytearray([0b00000010]))
 
 while True:
-    ## nothing yet i just want to test the connection
-    time.sleep(10); 
-    s.send(bytearray([0b11111111, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000]))
+    events = inputs.get_gamepad()
+    
+    for event in events:
+        if 'eventType' == 'BUTTON':
+            if control == '1':
+                if value:
+                    s.send(bytearray([0b11000001]))
+                else:
+                    s.send(bytearray([0b11100001]))
+            if control == '2':
+                if value:
+                    s.send(bytearray([0b11000010]))
+                else:
+                    s.send(bytearray([0b11100010]))
+            if control == '3':
+                if value:
+                    s.send(bytearray([0b11000011]))
+                else:
+                    s.send(bytearray([0b11100011]))
+            if control == '4':
+                if value:
+                    s.send(bytearray([0b11000100]))
+                else:
+                    s.send(bytearray([0b11100100]))
+            if control == '5':
+                if value:
+                    s.send(bytearray([0b11000101]))
+                else:
+                    s.send(bytearray([0b11100101]))
+            if control == '6':
+                if value:
+                    s.send(bytearray([0b11000110]))
+                else:
+                    s.send(bytearray([0b11100110]))
+            if control == '7':
+                if value:
+                    s.send(bytearray([0b11000111]))
+                else:
+                    s.send(bytearray([0b11100111]))
+            if control == 'E1':
+                if value:
+                    s.send(bytearray([0b11001000]))
+                else:
+                    s.send(bytearray([0b11101000]))
+            if control == 'E2':
+                if value:
+                    s.send(bytearray([0b11001001]))
+                else:
+                    s.send(bytearray([0b11101001]))
+
+    time.sleep(0.01)
     pass
--- iidx_hack_server.py
+++ iidx_hack_server.py
@@ -1,4 +1,4 @@
-import socketserver
+import socketserver, vgamepad
 
 ## Server runs on the host Windows machine and creates a virtual gamepad
 
@@ -6,10 +6,64 @@ class IIDXHackTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         while True:
             self.data = self.request.recv(49)
-            if self.data[0] == 0b00000001:
+            if self.data[0] == 0b00000001: #connection state change
                 print("[status] a connection has been successfully established")
+            if self.data[0] == 0b00000010: #controller connected
+                print("[status] controller connected")
+                self.gamepad = vgamepad.VDS4Gamepad()
+                                    
+                self.gamepad.update()
+            if self.data[0] == 0b00000011: #disconnected
+                self.gamepad = None
             if self.data[0] == 0b11111111:
                 print("[status] recieved 128 (you are either doing this on purpose or have encountered an error)")
+            # i dont understand bytes so i will just hardcore this for now. i'm sorry. it works.
+            ## PRESS EVENTS
+            if self.data[0] == 0b11000001: # press key 1
+                self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_CROSS)
+            if self.data[0] == 0b11000010: # press key 2
+                self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_CIRCLE)
+            if self.data[0] == 0b11000011: # press key 3
+                self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SQUARE)
+            if self.data[0] == 0b11000100: # press key 4
+                self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_TRIANGLE)
+            if self.data[0] == 0b11000101: # press key 5
+                self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SHOULDER_LEFT)
+            if self.data[0] == 0b11000110: # press key 6
+                self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SHOULDER_RIGHT)
+            if self.data[0] == 0b11000111: # press key 7
+                ## assigned to what is button 10 in the HTML5 gamepad api; I cannot seem to activate
+                ## button 6, what it should be, with vgamepad
+                self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_THUMB_LEFT)
+            if self.data[0] == 0b11001000: # e1
+                self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SHARE)
+            if self.data[0] == 0b11001001: # e2
+                self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_OPTIONS)
+            ## UNPRESS EVENTS
+            if self.data[0] == 0b11100001: # press key 1
+                self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_CROSS)
+            if self.data[0] == 0b11100010: # press key 2
+                self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_CIRCLE)
+            if self.data[0] == 0b11100011: # press key 3
+                self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SQUARE)
+            if self.data[0] == 0b11100100: # press key 4
+                self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_TRIANGLE)
+            if self.data[0] == 0b11100101: # press key 5
+                self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SHOULDER_LEFT)
+            if self.data[0] == 0b11100110: # press key 6
+                self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SHOULDER_RIGHT)
+            if self.data[0] == 0b11100111: # press key 7
+                ## assigned to what is button 10 in the HTML5 gamepad api; I cannot seem to activate
+                ## button 6, what it should be, with vgamepad
+                self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_THUMB_LEFT)
+            if self.data[0] == 0b11101000: # e1
+                self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SHARE)
+            if self.data[0] == 0b11101001: # e2
+                self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_OPTIONS)
+            ## Turntable
+            if self.data[0] == 0b11110000: # turntable axis in the next 4 bytes
+                pass
+            self.gamepad.update()
         pass
 
 with socketserver.TCPServer(('127.0.0.1', 44717), IIDXHackTCPHandler) as server:
