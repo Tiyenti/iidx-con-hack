2cbf1ca737e903d5d4c68d7d7bab94e58394396e
Tiyenti <11019859+Tiyenti@users.noreply.github.com>
2022-09-18 17:05:13 +0100

    messages are now null terminated (untested atm) also changed the buffer size from 49 to 64. i have no idea where i got 64 from. i might reduce it to 32 since that might be better in terms of latency but idk

--- iidx_hack_client.py
+++ iidx_hack_client.py
@@ -7,7 +7,7 @@ s.connect(("10.0.2.2", 44717))
 
 ## send some data to indicate we've connected
                  # status      1-7 and start e1 and e2     turntable axis float
-s.send(bytearray([0b00000001, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000]))
+s.send(bytearray([0b00000001, 0b00000000]))
 print("Successfully established connection to server.")
 
 a = 0
@@ -16,7 +16,7 @@ print("Awaiting controller connection...")
 
 inputs.get_gamepad()
 
-s.send(bytearray([0b00000010]))
+s.send(bytearray([0b00000010, 0b00000000]))
 
 # codes
 KEY1 = 'BTN_TRIGGER'
@@ -36,51 +36,51 @@ while True:
     for event in events:
         if event.code == KEY1:
             if event.state:
-                s.send(bytearray([0b11000001]))
+                s.send(bytearray([0b11000001, 0b00000000]))
             else:
-                s.send(bytearray([0b11100001]))
+                s.send(bytearray([0b11100001, 0b00000000]))
         if event.code == KEY2:
             if event.state:
-                s.send(bytearray([0b11000010]))
+                s.send(bytearray([0b11000010, 0b00000000]))
             else:
-                s.send(bytearray([0b11100010]))
+                s.send(bytearray([0b11100010, 0b00000000]))
         if event.code == KEY3:
             if event.state:
-                s.send(bytearray([0b11000011]))
+                s.send(bytearray([0b11000011, 0b00000000]))
             else:
-                s.send(bytearray([0b11100011]))
+                s.send(bytearray([0b11100011, 0b00000000]))
         if event.code == KEY4:
             if event.state:
-                s.send(bytearray([0b11000100]))
+                s.send(bytearray([0b11000100, 0b00000000]))
             else:
-                s.send(bytearray([0b11100100]))
+                s.send(bytearray([0b11100100, 0b00000000]))
         if event.code == KEY5:
             if event.state:
-                s.send(bytearray([0b11000101]))
+                s.send(bytearray([0b11000101, 0b00000000]))
             else:
-                s.send(bytearray([0b11100101]))
+                s.send(bytearray([0b11100101, 0b00000000]))
         if event.code == KEY6:
             if event.state:
-                s.send(bytearray([0b11000110]))
+                s.send(bytearray([0b11000110, 0b00000000]))
             else:
-                s.send(bytearray([0b11100110]))
+                s.send(bytearray([0b11100110, 0b00000000]))
         if event.code == KEY7:
             if event.state:
-                s.send(bytearray([0b11000111]))
+                s.send(bytearray([0b11000111, 0b00000000]))
             else:
-                s.send(bytearray([0b11100111]))
+                s.send(bytearray([0b11100111, 0b00000000]))
         if event.code == E1:
             if event.state:
-                s.send(bytearray([0b11001000]))
+                s.send(bytearray([0b11001000, 0b00000000]))
             else:
-                s.send(bytearray([0b11101000]))
+                s.send(bytearray([0b11101000, 0b00000000]))
         if event.code == E2:
             if event.state:
-                s.send(bytearray([0b11001001]))
+                s.send(bytearray([0b11001001, 0b00000000]))
             else:
-                s.send(bytearray([0b11101001]))
+                s.send(bytearray([0b11101001, 0b00000000]))
         if event.code == 'ABS_X':
-            s.send(bytearray([0b11110000, event.state + 127]))
+            s.send(bytearray([0b11110000, event.state + 127, 0b00000000]))
 
     #time.sleep(0.01)
     pass
--- iidx_hack_server.py
+++ iidx_hack_server.py
@@ -5,73 +5,89 @@ import socketserver, vgamepad
 class IIDXHackTCPHandler(socketserver.BaseRequestHandler):
     def handle(self):
         self.gamepad = None
+        
+        self.data = self.request.recv(64)
+        self.dataindex = 0
+        self.curmessage = []
         while True:
-            self.data = self.request.recv(49)
-            if self.data[0] == 0b00000001: #connection state change
-                print("[status] a connection has been successfully established")
-            if self.data[0] == 0b00000010: #controller connected
-                print("[status] controller connected")
-                self.gamepad = vgamepad.VDS4Gamepad()
-                                    
-                self.gamepad.update()
-            if self.data[0] == 0b00000011: #disconnected
-                self.gamepad = None
-            if self.data[0] == 0b11111111:
-                print("[status] recieved 128 (you are either doing this on purpose or have encountered an error)")
-            # i dont understand bytes so i will just hardcore this for now. i'm sorry. it works.
-            ## PRESS EVENTS
-            if self.data[0] == 0b11000001: # press key 1
-                self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_CROSS)
-            if self.data[0] == 0b11000010: # press key 2
-                self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_CIRCLE)
-            if self.data[0] == 0b11000011: # press key 3
-                self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SQUARE)
-            if self.data[0] == 0b11000100: # press key 4
-                self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_TRIANGLE)
-            if self.data[0] == 0b11000101: # press key 5
-                self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SHOULDER_LEFT)
-            if self.data[0] == 0b11000110: # press key 6
-                self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SHOULDER_RIGHT)
-            if self.data[0] == 0b11000111: # press key 7
-                ## vgamepad doesn't seem to have a way to press Button 7, at least not in a way
-                ## that the HTML5 Gamepad API will see, which bothers me. I could use the left
-                ## trigger, but using an axis for a key input seems to cause major problems in
-                ## beatoraja so I'm opting to use the left thumbstick button instead.
-                ## I hope this doesn't cause problems with Infinitas lmao but I believe you
-                ## can set your keybindings in that game so hopefully it wouldn't be a problem.
-                self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_THUMB_LEFT)
-            if self.data[0] == 0b11001000: # e1
-                self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SHARE)
-            if self.data[0] == 0b11001001: # e2
-                self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_OPTIONS)
-            ## UNPRESS EVENTS
-            if self.data[0] == 0b11100001: # press key 1
-                self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_CROSS)
-            if self.data[0] == 0b11100010: # press key 2
-                self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_CIRCLE)
-            if self.data[0] == 0b11100011: # press key 3
-                self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SQUARE)
-            if self.data[0] == 0b11100100: # press key 4
-                self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_TRIANGLE)
-            if self.data[0] == 0b11100101: # press key 5
-                self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SHOULDER_LEFT)
-            if self.data[0] == 0b11100110: # press key 6
-                self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SHOULDER_RIGHT)
-            if self.data[0] == 0b11100111: # press key 7
-                ## see the equivalent comment in the press section
-                self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_THUMB_LEFT)
-            if self.data[0] == 0b11101000: # e1
-                self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SHARE)
-            if self.data[0] == 0b11101001: # e2
-                self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_OPTIONS)
-            ## Turntable
-            if self.data[0] == 0b11110000: # turntable axis in the next 4 bytes
-                ## raw value is -127 to 127, it seems; on the client end I add
-                ## 127 to the value so it's within the range of 0-254 so i can just
-                ## send the raw int as a byte.  
-                self.gamepad.left_joystick(self.data[1], 128)
-            if not self.gamepad == None:
-                self.gamepad.update()
+            if self.dataindex == len(self.data):
+                self.data = self.request.recv(64)
+                self.dataindex = 0
+
+            for msgbyte in self.data:
+                if msgbyte != 0x00:
+                    self.curmessage.append(msgbyte)
+                else:
+                    self.readMessage(self.curmessage)
+                    self.curmessage = []
+                self.dataindex += 0            
+    
+    def readMessage(self, message):
+        if message[0] == 0b00000001: #connection state change
+            print("[status] a connection has been successfully established")
+        if message[0] == 0b00000010: #controller connected
+            print("[status] controller connected")
+            self.gamepad = vgamepad.VDS4Gamepad()
+                                
+            self.gamepad.update()
+        if message[0] == 0b00000011: #disconnected
+            self.gamepad = None
+        if message[0] == 0b11111111:
+            print("[status] recieved 128 (you are either doing this on purpose or have encountered an error)")
+        # i dont understand bytes so i will just hardcore this for now. i'm sorry. it works.
+        ## PRESS EVENTS
+        if message[0] == 0b11000001: # press key 1
+            self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_CROSS)
+        if message[0] == 0b11000010: # press key 2
+            self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_CIRCLE)
+        if message[0] == 0b11000011: # press key 3
+            self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SQUARE)
+        if message[0] == 0b11000100: # press key 4
+            self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_TRIANGLE)
+        if message[0] == 0b11000101: # press key 5
+            self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SHOULDER_LEFT)
+        if message[0] == 0b11000110: # press key 6
+            self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SHOULDER_RIGHT)
+        if message[0] == 0b11000111: # press key 7
+            ## vgamepad doesn't seem to have a way to press Button 7, at least not in a way
+            ## that the HTML5 Gamepad API will see, which bothers me. I could use the left
+            ## trigger, but using an axis for a key input seems to cause major problems in
+            ## beatoraja so I'm opting to use the left thumbstick button instead.
+            ## I hope this doesn't cause problems with Infinitas lmao but I believe you
+            ## can set your keybindings in that game so hopefully it wouldn't be a problem.
+            self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_THUMB_LEFT)
+        if message[0] == 0b11001000: # e1
+            self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SHARE)
+        if message[0] == 0b11001001: # e2
+            self.gamepad.press_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_OPTIONS)
+        ## UNPRESS EVENTS
+        if message[0] == 0b11100001: # press key 1
+            self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_CROSS)
+        if message[0] == 0b11100010: # press key 2
+            self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_CIRCLE)
+        if message[0] == 0b11100011: # press key 3
+            self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SQUARE)
+        if message[0] == 0b11100100: # press key 4
+            self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_TRIANGLE)
+        if message[0] == 0b11100101: # press key 5
+            self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SHOULDER_LEFT)
+        if message[0] == 0b11100110: # press key 6
+            self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SHOULDER_RIGHT)
+        if message[0] == 0b11100111: # press key 7
+            ## see the equivalent comment in the press section
+            self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_THUMB_LEFT)
+        if message[0] == 0b11101000: # e1
+            self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_SHARE)
+        if message[0] == 0b11101001: # e2
+            self.gamepad.release_button(button=vgamepad.DS4_BUTTONS.DS4_BUTTON_OPTIONS)
+        ## Turntable
+        if message[0] == 0b11110000: # turntable axis in the next 4 bytes
+            ## raw value is -127 to 127, it seems; on the client end I add
+            ## 127 to the value so it's within the range of 0-254 so i can just
+            ## send the raw int as a byte.  
+            self.gamepad.left_joystick(message[1], 128)
+        if not self.gamepad == None:
+            self.gamepad.update()
         pass
 
 with socketserver.TCPServer(('127.0.0.1', 44717), IIDXHackTCPHandler) as server:
